b9e8f49704eb4fbe7d61fe856096890a70edbb97
Media Davarkhah <Media.dvh@gmail.com>
2020-11-14 14:40:58 +0330

    Initial Commit

---
+++ communication FlashCards for German/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  communication FlashCards for German
+//
+//  Created by Media Davarkhah on 8/24/1399 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
